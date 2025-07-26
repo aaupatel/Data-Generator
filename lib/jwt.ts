a66b9835912670df08ee5db05d8d532e2d86fe10
Ayush Patidar <appatel00016@gmail.com>
2025-07-26 21:04:38 +0530

    fix: JWT_SECRET error of undefined

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const generateToken = (payload: any) => {
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is undefined");
+  }
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 };
+
 
 export const verifyToken = (token: string) => {
   try {
